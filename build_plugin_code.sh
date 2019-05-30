d5bf54f95f82806b36990bba7d9c589f6f052135
sterry1 <sterry1@tacc.utexas.edu>
2019-05-30 08:55:23 -0500

    additional comments

--- build_plugin_code.sh
+++ build_plugin_code.sh
@@ -7,5 +7,5 @@
 # basic build and install for plugin artifacts
 mvn -f build-pom.xml clean install
 
-# move to Docker directory for building a new image
+# stage artifacts to Docker directory for building a new image
 cp -rf target/restheart-aloeplugin-1.0.jar Docker/
